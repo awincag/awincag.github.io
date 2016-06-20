59c37bdabc9004b5662305e70c6bc09543333380
awincag <alex.cagwin@gmail.com>
2016-06-19 20:15:42 -0700

    Update to Site

--- js/main.js
+++ js/main.js
@@ -1,70 +1,48 @@
-document.getElementById("copyButton2").addEventListener("click", function() {
-    copyToClipboardMsg(document.getElementById("copyTarget2"), "msg");
+var btn = document.getElementById('btn');
+var message = document.getElementById('msg');
+var failMessage = document.getElementById('f-msg');
+var clipboard = new Clipboard(btn);
+
+for (var i; i < btn.length; i++) {
+
+};
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    e.clearSelection();
+
+    message.classList.toggle('content-open');
+    window.setTimeout(function() {
+      message.classList.remove('content-open');
+    }, 2500);
 });
 
-function copyToClipboardMsg(elem, msgElem) {
-	  var succeed = copyToClipboard(elem);
-    var msg;
-    if (!succeed) {
-        msg = "Copy not supported or blocked.  Press Ctrl+c to copy."
-    } else {
-        msg = "Text copied to the clipboard."
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+
+    failMessage.classList.toggle('content-open');
+    window.setTimeout(function() {
+      failMessage.classList.remove('content-open');
+    }, 5500);
+});
+
+var message = document.getElementById('msg');
+var failMessage = document.getElementById('f-msg');
+var close = document.getElementsByClassName("closebtn");
+var i;
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        message.classList.remove('content-open');
     }
-    if (typeof msgElem === "string") {
-        msgElem = document.getElementById(msgElem);
-    }
-    msgElem.innerHTML = msg;
-    setTimeout(function() {
-        msgElem.innerHTML = "";
-    }, 2000);
 }
-
-function copyToClipboard(elem) {
-	  // create hidden text element, if it doesn't already exist
-    var targetId = "_hiddenCopyText_";
-    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-    var origSelectionStart, origSelectionEnd;
-    if (isInput) {
-        // can just use the original source element for the selection and copy
-        target = elem;
-        origSelectionStart = elem.selectionStart;
-        origSelectionEnd = elem.selectionEnd;
-    } else {
-        // must use a temporary form element for the selection and copy
-        target = document.getElementById(targetId);
-        if (!target) {
-            var target = document.createElement("textarea");
-            target.style.position = "absolute";
-            target.style.left = "-9999px";
-            target.style.top = "0";
-            target.id = targetId;
-            document.body.appendChild(target);
-        }
-        target.textContent = elem.textContent;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        failMessage.classList.remove('content-open');
     }
-    // select the content
-    var currentFocus = document.activeElement;
-    target.focus();
-    target.setSelectionRange(0, target.value.length);
-
-    // copy the selection
-    var succeed;
-    try {
-    	  succeed = document.execCommand("copy");
-    } catch(e) {
-        succeed = false;
-    }
-    // restore original focus
-    if (currentFocus && typeof currentFocus.focus === "function") {
-        currentFocus.focus();
-    }
-
-    if (isInput) {
-        // restore prior selection
-        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
-    } else {
-        // clear temporary content
-        target.textContent = "";
-    }
-    return succeed;
 }
